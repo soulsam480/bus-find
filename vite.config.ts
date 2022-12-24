018f82c9e2cc126d05108afab70c7b6259c787e6
soulsam480 <soulsam480@hotmail.com>
2022-12-24 14:08:53 +0530

    feat: make app a PWA

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue';
 import Unocss from 'unocss/vite';
 import presetWind from '@unocss/preset-wind';
 import { presetForms } from '@julr/unocss-preset-forms';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +11,30 @@ export default defineConfig({
     vue(),
     Unocss({
       presets: [presetWind(), presetForms()],
+    }),
+    VitePWA({
+      strategies: 'generateSW',
+      injectRegister: 'auto',
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+        sourcemap: false,
+      },
+      manifest: {
+        name: 'Find Bus',
+        short_name: 'Find Bus',
+        description: 'Find Bus in Bengaluru',
+        theme_color: '#CEE5A8',
+        icons: [
+          ...[48, 72, 98, 144, 192, 512].map((size) => {
+            return {
+              src: `icon-${size}-${size}.png`,
+              sizes: `${size}x${size}`,
+              type: 'image/png',
+            };
+          }),
+        ],
+      },
     }),
   ],
 });
